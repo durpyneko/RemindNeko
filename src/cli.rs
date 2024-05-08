d5cb5ecb446199e7984cd1ba19b061458ccddbcb
durpyneko <89787577+durpyneko@users.noreply.github.com>
2024-05-08 22:26:31 +0200

    finished some functions

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,19 @@
 use clap::{Parser, Subcommand};
+use colored::*;
 
+fn banner() -> String {
+    let banner = "
+    ██████╗ ███████╗███╗   ███╗██╗███╗   ██╗██████╗ ███╗   ██╗███████╗██╗  ██╗ ██████╗ 
+    ██╔══██╗██╔════╝████╗ ████║██║████╗  ██║██╔══██╗████╗  ██║██╔════╝██║ ██╔╝██╔═══██╗     ╱|、
+    ██████╔╝█████╗  ██╔████╔██║██║██╔██╗ ██║██║  ██║██╔██╗ ██║█████╗  █████╔╝ ██║   ██║    (˚ˎ 。7
+    ██╔══██╗██╔══╝  ██║╚██╔╝██║██║██║╚██╗██║██║  ██║██║╚██╗██║██╔══╝  ██╔═██╗ ██║   ██║     |、˜〵
+    ██║  ██║███████╗██║ ╚═╝ ██║██║██║ ╚████║██████╔╝██║ ╚████║███████╗██║  ██╗╚██████╔╝     じしˍ,)ノ
+    ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝ 
+    ";
+    banner.color(Color::Magenta).to_string()
+}
 #[derive(Debug, Parser)]
-#[command(name = "RemindNeko")]
-#[command(about = "RemindNeko *should* remind you")]
+#[command(name = "RemindNeko", about = banner())]
 pub struct Cli {
     #[command(subcommand)]
     pub command: CliSubcommand,
@@ -10,14 +21,17 @@ pub struct Cli {
 
 #[derive(Debug, Subcommand)]
 pub enum CliSubcommand {
-    #[command(about = "set a reminder")]
-    Set { name: String, desc: String },
+    #[command(about = "set a reminder", alias = "new", alias = "n", alias = "add")]
+    Set { name: String, time: String },
 
-    #[command(about = "delete a reminder")]
+    #[command(about = "delete a reminder", alias = "rm", alias = "r")]
     Remove { name: String },
 
     #[command(about = "configure settings")]
     Settings { name: String, conf: String },
+
+    #[command(about = "print current reminders")]
+    List,
 
     #[command(about = "start a reminder")]
     Start { name: String },
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,216 @@
+/*
+
+    ██████╗ ███████╗███╗   ███╗██╗███╗   ██╗██████╗ ███╗   ██╗███████╗██╗  ██╗ ██████╗
+    ██╔══██╗██╔════╝████╗ ████║██║████╗  ██║██╔══██╗████╗  ██║██╔════╝██║ ██╔╝██╔═══██╗     ╱|、
+    ██████╔╝█████╗  ██╔████╔██║██║██╔██╗ ██║██║  ██║██╔██╗ ██║█████╗  █████╔╝ ██║   ██║    (˚ˎ 。7
+    ██╔══██╗██╔══╝  ██║╚██╔╝██║██║██║╚██╗██║██║  ██║██║╚██╗██║██╔══╝  ██╔═██╗ ██║   ██║     |、˜〵
+    ██║  ██║███████╗██║ ╚═╝ ██║██║██║ ╚████║██████╔╝██║ ╚████║███████╗██║  ██╗╚██████╔╝     じしˍ,)ノ
+    ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝
+
+*/
+
 mod cli;
 
 use chrono::prelude::*;
+use chrono::Duration;
 use clap::Parser;
 use cli::*;
+use colored::*;
+use colored_json::prelude::*;
+use colored_json::to_colored_json;
+use humantime::format_duration;
+use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::sync::Mutex;
 
-const REMINDERS_FILE: &str = "reminders.json";
-static mut CONFIG: Config = Config {
-    reminders: Vec::new(),
-    settings: Vec::new(),
-};
+// yo this regex!?!?
+macro_rules! log {
+    ($level:expr, $message:expr) => {{
+        let symbol_color = match $level {
+            "+" => "green",
+            "-" => "yellow",
+            "!" => "red",
+            _ => "white",
+        };
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Reminder {
-    time: u32,
-    name: String,
-    description: String,
+        println!(
+            "{}{}{} {}",
+            "[".bright_black(),
+            $level.color(symbol_color),
+            "]".bright_black(),
+            $message
+        )
+    }};
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {{
+        log!("+", $($arg)*)
+    }};
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {{
+        log!("-", $($arg)*)
+    }};
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {{
+        log!("!", $($arg)*)
+    }};
+}
+
+const REMINDERS_FILE: &str = "REMIND_NEKO.json";
+
+static CONFIG: OnceCell<Mutex<Config>> = OnceCell::new();
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Reminder {
+    name: String,
+    time: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Settings {
     name: String,
-    description: String,
+    time: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Config {
     reminders: Vec<Reminder>,
     settings: Vec<Settings>,
 }
 
 fn load_reminders() {
-    let data = std::fs::read_to_string(&REMINDERS_FILE).unwrap();
+    let data = match fs::read_to_string(&REMINDERS_FILE) {
+        Ok(data) => data,
+        Err(_) => {
+            let empty_config = Config {
+                reminders: Vec::new(),
+                settings: Vec::new(),
+            };
+            let json = serde_json::to_string_pretty(&empty_config).unwrap();
+            fs::write(&REMINDERS_FILE, json).expect("Failed to create reminders file");
+            return;
+        }
+    };
     let config = match serde_json::from_str::<Config>(&data) {
         Ok(config) => config,
-        Err(_) => Config {
-            reminders: Vec::new(),
-            settings: Vec::new(),
-        },
+        Err(_) => {
+            error!("Invalid JSON format in reminders file. Using empty config.");
+            Config {
+                reminders: Vec::new(),
+                settings: Vec::new(),
+            }
+        }
     };
-    unsafe { CONFIG = config };
+    CONFIG.set(Mutex::new(config)).unwrap()
 }
 
 fn save_reminders() {
-    unsafe {
-        std::fs::write(
-            &REMINDERS_FILE,
-            serde_json::to_string_pretty(&CONFIG).unwrap(),
-        )
-        .unwrap();
-    }
+    let config = CONFIG.get().unwrap().lock().unwrap().clone();
+    std::fs::write(
+        &REMINDERS_FILE,
+        serde_json::to_string_pretty(&config).unwrap(),
+    )
+    .unwrap();
+    info!("Updated config!")
 }
 
-// let mut config = Config {
-//     reminders: Vec::new(),
-// };
-// let reminder = Reminder {
-//     time: 0,
-//     name: String::from("Example name"),
-//     description: String::from("Example description"),
-// };
-// config.reminders.push(reminder);
-// std::fs::write(
-//     "reminders.json",
-//     serde_json::to_string_pretty(&config).unwrap(),
-// )
-// .unwrap();
-
-fn new_reminder(name: String, desc: String) {
-    println!("Setting reminder: name={}, description={}", name, desc);
+fn new_reminder(name: String, time_str: String) {
     let local: DateTime<Local> = Local::now();
-    let time_as_u32 = local.timestamp() as u32;
-    unsafe {
-        CONFIG.reminders.push(Reminder {
-            time: time_as_u32,
-            name,
-            description: desc,
-        })
+    let duration = parse_duration(&time_str);
+    let reminder_time = local + duration;
+    let time_left = reminder_time.signed_duration_since(local);
+
+    // handle result otherwise it shits itself
+    // mf i wanted to have an 1 liner (ò_óˇ)_╭∩╮
+    let std_time_left = match time_left.to_std() {
+        Ok(std_duration) => std_duration,
+        Err(e) => {
+            error!(format!("Error converting duration: {}", e));
+            return;
+        }
     };
+
+    info!(format!(
+        "Setting reminder '{}'. Time left: {} hours ({})",
+        name,
+        time_left.num_hours(),
+        format_duration(std_time_left).to_string()
+    ));
+
+    let formatted_time = reminder_time.format("%Y-%m-%d %H:%M:%S").to_string();
+
+    let mut config = CONFIG.get().unwrap().lock().unwrap();
+    config.reminders.push(Reminder {
+        name: name,
+        time: formatted_time,
+    });
+    drop(config);
     save_reminders();
 }
 
+// parse duration from a string like "5hours" or "3days1hour5minutes"
+fn parse_duration(input: &str) -> Duration {
+    let mut duration = Duration::zero();
+    let mut current_number = String::new();
+    for c in input.chars() {
+        if c.is_digit(10) {
+            current_number.push(c);
+        } else {
+            let value = current_number.parse::<i64>().unwrap_or(0);
+            current_number.clear();
+            match c {
+                'd' => duration = duration + Duration::days(value),
+                'h' => duration = duration + Duration::hours(value),
+                'm' => duration = duration + Duration::minutes(value),
+                _ => {}
+            }
+        }
+    }
+    duration
+}
+
 fn remove_reminder(name: String) {
-    let removed = unsafe { CONFIG.reminders.iter().position(|r| r.name == name) };
+    let mut config = CONFIG.get().unwrap().lock().unwrap();
+    let removed = config.reminders.iter().position(|r| r.name == name);
 
     if let Some(index) = removed {
-        println!("Removing reminder: name={}", name);
-        unsafe {
-            CONFIG.reminders.remove(index);
-        }
+        info!(format!("Removing reminder: {}", name));
+        config.reminders.remove(index);
+        drop(config);
         save_reminders();
     } else {
-        println!("Reminder not found.");
+        error!("Reminder not found!")
+    }
+}
+
+fn list_reminders() {
+    let config = CONFIG.get().unwrap().lock().unwrap().clone();
+
+    info!(format!(
+        "Config:\n{}",
+        to_colored_json(&config, ColorMode::On).unwrap()
+    ));
+}
+
+fn start_reminder(name: String) {
+    info!(format!("Starting reminder: {}", name));
+
+    let config = CONFIG.get().unwrap().lock().unwrap().clone();
+    let matched_conf = config.reminders.iter().position(|r| r.name == name);
+
+    if let Some(index) = matched_conf {
+        info!(format!("Matched '{}' at index: {}", name, index));
+    } else {
+        error!("Reminder not found!");
     }
 }
 
@@ -100,20 +219,28 @@ fn main() {
     load_reminders();
 
     match args.command {
-        CliSubcommand::Set { name, desc } => {
-            new_reminder(name, desc);
+        CliSubcommand::Set { name, time } => {
+            new_reminder(name, time);
         }
         CliSubcommand::Remove { name } => {
             remove_reminder(name);
         }
         CliSubcommand::Settings { name, conf } => {
-            println!("Setting settings: name={}, config={}", name, conf);
+            info!(format!("Setting settings: name={}, config={}", name, conf));
+        }
+        CliSubcommand::List => {
+            list_reminders();
         }
         CliSubcommand::Start { name } => {
-            println!("Starting reminder: name={}", name);
+            start_reminder(name);
         }
         CliSubcommand::Stop { name } => {
-            println!("Stopping reminder: name={}", name);
+            info!(format!("Stopping reminder: {}", name));
         }
     }
 }
+/*
+  ∧,,,∧
+(  ̳• · • ̳)
+/    づ♡ read if cute
+*/
